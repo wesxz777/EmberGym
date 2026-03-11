d2ac4d97e77b5a7bdb3980e4c028dced72fdbc2d
Wesley Xyron Caya <wcaya.3884@umak.edu.ph>
2026-03-11 19:58:21 +0800

    Updated base for the vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/',
+  base: '/EmberGym/',
   server: {
     proxy: {
       '/api': {

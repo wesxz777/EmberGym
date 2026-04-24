777d212964bb01ee1175b42bc57798c0163eb192
Wesley Xyron Caya <wcaya.3884@umak.edu.ph>
2026-04-24 18:03:36 +0800

    Fix white screen and routing

--- src/app/utils/api.ts
+++ src/app/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://embergym.onrender.com/api",
   headers: { Accept: "application/json", "Content-Type": "application/json" },
 });
 

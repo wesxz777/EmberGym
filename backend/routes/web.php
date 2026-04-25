b333eaefd923d37bda27f4260e22553a0fb13b85
Wesley Xyron Caya <wcaya.3884@umak.edu.ph>
2026-04-25 09:13:28 +0800

    Change backdoor to migrate fresh

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,12 @@
-<?php
-
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/force-migrate', function () {
+    try {
+        // 🔥 Notice the :fresh added here! 🔥
+        Artisan::call('migrate:fresh', ['--force' => true]);
+        return response()->json(['message' => 'Database completely rebuilt successfully!']);
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()]);
+    }
 });
